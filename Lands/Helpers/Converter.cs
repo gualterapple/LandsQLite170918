fe716d686f2b0c250833e6c8a8daa9cb2c6acf2d
MAC <mac@MacBook-Pro-de-Gualter.local>
2018-10-17 15:46:32 +0100

    My Profile

--- Lands/Helpers/Converter.cs
+++ Lands/Helpers/Converter.cs
@@ -23,5 +23,20 @@ namespace Lands.Helpers
                 UserTypeId = user.UserTypeId,
             };
     }
+
+        internal static User ToUserDomain(UserLocal user, byte[] imageArray)
+        {
+            return new User
+            {
+                Email = user.Email,
+                FirstName = user.FirstName,
+                ImagePath = user.ImagePath,
+                LastName = user.LastName,
+                Telephone = user.Telephone,
+                UserId = user.UserId,
+                UserTypeId = user.UserTypeId.Value,
+                ImageArray = imageArray,
+            };
+    }
     }
 }
--- Lands/ViewModels/MyProfileViewModel.cs
+++ Lands/ViewModels/MyProfileViewModel.cs
@@ -62,6 +62,172 @@
             this.IsEnabled = true;
         }
         #endregion
+        #region Commands
+        public ICommand ChangeImageCommand
+        {
+            get
+            {
+                return new RelayCommand(ChangeImage);
+            }
+        }
+
+        private async void ChangeImage()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (CrossMedia.Current.IsCameraAvailable &&
+                CrossMedia.Current.IsTakePhotoSupported)
+            {
+                var source = await Application.Current.MainPage.DisplayActionSheet(
+                    Languages.SourceImageQuestion,
+                    Languages.Cancel,
+                    null,
+                    Languages.FromGallery,
+                    Languages.FromCamera);
+
+                if (source == Languages.Cancel)
+                {
+                    this.file = null;
+                    return;
+                }
+
+                if (source == Languages.FromCamera)
+                {
+                    this.file = await CrossMedia.Current.TakePhotoAsync(
+                        new StoreCameraMediaOptions
+                        {
+                            Directory = "Sample",
+                            Name = "test.jpg",
+                            PhotoSize = PhotoSize.Small,
+                        }
+                    );
+                }
+                else
+                {
+                    this.file = await CrossMedia.Current.PickPhotoAsync();
+                }
+            }
+            else
+            {
+                this.file = await CrossMedia.Current.PickPhotoAsync();
+            }
+
+            if (this.file != null)
+            {
+                this.ImageSource = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });
+            }
+        }
+
+        public ICommand SaveCommand
+        {
+            get
+            {
+                return new RelayCommand(Save);
+            }
+        }
+
+        private async void Save()
+        {
+            if (string.IsNullOrEmpty(this.User.FirstName))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.FirstNameValidation,
+                    Languages.Accept);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.User.LastName))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.LastNameValidation,
+                    Languages.Accept);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.User.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.EmailValidation,
+                    Languages.Accept);
+                return;
+            }
+
+            if (!RegexUtilities.IsValidEmail(this.User.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.EmailValidation2,
+                    Languages.Accept);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.User.Telephone))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.PhoneValidation,
+                    Languages.Accept);
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            var checkConnetion = await this.apiService.CheckConnection();
+            if (!checkConnetion.IsSuccess)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    checkConnetion.Message,
+                    Languages.Accept);
+                return;
+            }
+
+            byte[] imageArray = null;
+            if (this.file != null)
+            {
+                imageArray = FilesHelper.ReadFully(this.file.GetStream());
+            }
+
+            var userDomain = Converter.ToUserDomain(this.User, imageArray);
+
+            var apiSecurity = Application.Current.Resources["APISecurity"].ToString();
+            var response = await this.apiService.Put(
+                apiSecurity,
+                "/api",
+                "/Users",
+                MainViewModel.GetInstance().TokenType,
+                MainViewModel.GetInstance().Token,
+                userDomain);
+
+            if (!response.IsSuccess)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    response.Message,
+                    Languages.Accept);
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            
+            await App.Navigator.PopAsync();
+        }
+
+        #endregion  
     }
 }
 
